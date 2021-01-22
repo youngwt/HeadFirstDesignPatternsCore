8d824689841c885b84e3bddf20259086939e1735
Wayne Atkinson Young <youngwt@gmail.com>
2021-01-22 22:40:08 +0000

    Added new projects for Chapter 4 - Factory Pattern

---
+++ HeadFirstDesignPatterns/FactoryPatternTests/FactoryPatternTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace FactoryPatternTests
+{
+    public class FactoryPatternTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
